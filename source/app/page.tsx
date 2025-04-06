4f78aed2e65aad16484e56c1356d9b9a0a148a01
ATarjun007 <atarjun2000@gmail.com>
2025-04-06 20:28:19 +0530

    code add

--- source/app/page.tsx
+++ source/app/page.tsx
@@ -2,11 +2,14 @@ import { Hero } from "@/components/Hero";
 import Sidebar from "@/components/Sidebar"
 
 
-export default function Home() {
+export default function Home() { 
+
   return (
    <div className="flex p-8">
     <Sidebar/>
     <Hero/>
+  
+
    </div>
   );
 }
--- source/components/Create.tsx
+++ source/components/Create.tsx
@@ -1,39 +1,49 @@
-"use client"
-import { title } from 'process';
-import React from 'react'
-import {useState} from 'react'
+"use client";
+import { title } from "process";
+import React from "react";
+import { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
 
-    const [title , setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [image, setImage] = useState("");
+  const post_data = () => {
+    //localStorage
+    localStorage.setItem("title", title)
+    localStorage.setItem("description", description)
+    localStorage.setItem("image", image)
+    console.log({ title, description, image });
+  };
 
-    const post_data = () => {
-        console.log({title, description, image});
-
-    
-    }
-   
-    
-
-    
-//e.target.value
+  //e.target.value
   return (
-    
-   <div>
+    <div>
+      <div className="">
+        <input
+          type="text"
+          placeholder="Add title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div className="">
+          <textarea
+            name="description"
+            placeholder="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+        <input
+          type="text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
 
-    <div className=''>
-        <input type="text" placeholder='Add title' value={title} onChange={(e) => setTitle(e.target.value)}/>      
-      <div className=''><textarea name="description" placeholder='description' value={description} onChange={(e)=> setDescription(e.target.value)}></textarea></div>
-      <input type="file" accept='image/png, image/jpg, image/jpeg, image/gif, image/mp4' value={image} onChange={(e)=> setImage(e.target.value)}/>
-
-       <button onClick={(e)=>  post_data()}>Add post</button>
+        <button onClick={(e) => post_data()}>Add post</button>
+      </div>
     </div>
-    
+  );
+};
 
-   </div>
-  )
-}
-
-export default Create
+export default Create;
